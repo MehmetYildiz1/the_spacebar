4550bd6bad733e71e89d85f9d726bd9f97a56201
Mehmet <302574757@student.rocmondriaan.nl>
2019-11-20 14:56:05 +0100

    done

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+
+    /**
+     * @Route("/news/why-asteroids-taste-like-bacon")
+     */
+    public function show()
+    {
+        return new Response('Future page to show one space article!');
+    }
+}
---
+++ src/Entity/Title.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TitleRepository")
+ */
+class Title
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $string;
+
+    /**
+     * @ORM\Column(type="string", length=100, unique=true)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publishedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getString(): ?string
+    {
+        return $this->string;
+    }
+
+    public function setString(string $string): self
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(?\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+}
